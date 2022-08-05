c32bce2da302be4ac156d09a01bcf982337eda8a
SollyJ <jeongpine14@gmail.com>
2022-08-05 13:05:05 +0900

    RecyclerView

--- app/src/main/java/com/example/moviebrowser/MainActivity.kt
+++ app/src/main/java/com/example/moviebrowser/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.moviebrowser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moviebrowser.Model.MovieDTO
 import com.example.moviebrowser.Service.MovieService
+import com.example.moviebrowser.adapter.MovieAdapter
 import com.example.moviebrowser.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import retrofit2.Call
@@ -15,38 +17,60 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var movieService: MovieService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initSearchButton()
+        initMovieListRecyclerView()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://openapi.naver.com")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        val movieService = retrofit.create(MovieService::class.java)
-        movieService.getMovieList(getString(R.string.clientId), getString(R.string.clientSecret), binding.searchEditText.toString())
+        movieService = retrofit.create(MovieService::class.java)
+
+    }
+
+    private fun initSearchButton() {
+        binding.searchButton.setOnClickListener() {
+            search(binding.searchEditText.text.toString())
+        }
+    }
+
+    // 검색 결과 보여주기 & 검색 기록 저장
+    private fun search(query: String) {
+        movieService.getMovieList(getString(R.string.clientId), getString(R.string.clientSecret), query)
             .enqueue(object: Callback<MovieDTO> {
                 override fun onResponse(call: Call<MovieDTO>, response: Response<MovieDTO>) {
+                    // todo 검색 기록 저장 함수 구현
+
                     if(response.isSuccessful.not()) {   // 예외처리
-                        Log.d("MainActivity", "FAIL")
+                        Log.d("MainActivity", "RESPONSE FAIL")
                         return
                     }
 
-                    // TODO 성공처리
+                    // 성공처리
                     response.body()?.let {
-                        it.movies.forEach { movie ->
-                            Log.d("MainActivity", movie.toString())
-                        }
+                        movieAdapter.submitList(it.movies)
                     }
                 }
 
                 override fun onFailure(call: Call<MovieDTO>, t: Throwable) {
-                    Log.d("MainActivity", "FAIL")
+                    Log.d("MainActivity", "FAIL!!")
                 }
 
             })
+    }
+
+    private fun initMovieListRecyclerView() {
+        movieAdapter = MovieAdapter()
+        binding.movieListRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.movieListRecyclerView.adapter = movieAdapter
     }
 }
---
+++ app/src/main/java/com/example/moviebrowser/adapter/MovieAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.moviebrowser.adapter
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.text.parseAsHtml
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviebrowser.Model.Movie
+import com.example.moviebrowser.databinding.ItemMovieBinding
+
+class MovieAdapter(): ListAdapter<Movie, MovieAdapter.MovieItemViewHolder>(diffUtil) {
+    inner class MovieItemViewHolder(private val binding: ItemMovieBinding): RecyclerView.ViewHolder(binding.root) {
+        @RequiresApi(Build.VERSION_CODES.N)
+        @SuppressLint("SetTextI18n")
+        fun bind(movie: Movie) {
+            binding.titleTextView.text = "제목: " + Html.fromHtml(movie.title, Html.FROM_HTML_MODE_LEGACY).toString()
+            binding.releaseDateTextView.text = "출시: " + movie.releaseDate
+            binding.ratingTextView.text = "평점: " + movie.rating
+
+            // todo Gilde 이미지 업로드
+            // todo 클릭이벤트
+        }
+    }
+
+    // 뷰홀더를 생성 해주는 함수
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieItemViewHolder {
+        return MovieItemViewHolder(ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    // 뷰홀더에 레이아웃 파일을 바인드 해주는 함수
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onBindViewHolder(holder: MovieItemViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object: DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
